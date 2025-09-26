77ef24f804230c2fcde06e913c5bac77fb5e19d5
Muhammed Ahmed Ashfaq <114571771+MDAhmed115@users.noreply.github.com>
2025-09-26 15:03:57 +0500

    fixed logical error

--- Lab-04 Tasks/airline.c
+++ Lab-04 Tasks/airline.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
+
 int main()
 {
-    char class[100];
+    char seatClass[100];
     int row;
     char children[100];
-    printf("Enter your class (buisness/econmy): ");
-    scanf("%s", &class);
-    printf("Enter your row nunber (1-21): ");
+
+    printf("Enter your class (business/economy): ");
+    scanf("%s", seatClass);
+
+    printf("Enter your row number (1-21): ");
     scanf("%d", &row);
+
     printf("Do you have children travelling with you (yes/no): ");
-    scanf("%s", &children);
-    if (strcmp(class, "buisness") == 0 || strcmp(children, "yes") == 0)
+    scanf("%s", children);
+
+    if (strcmp(seatClass, "business") == 0 || strcmp(children, "yes") == 0)
     {
         printf("You are in Group A!");
     }
-    else if (strcmp(class, "econmy") == 0 || (row >= 1 && row <= 10))
+    else if (strcmp(seatClass, "economy") == 0 && (row >= 1 && row <= 10))
     {
         printf("You are in Group B!");
     }
-    else if (strcmp(class, "econmy") == 0 || (row >= 11 && row <= 20))
+    else if (strcmp(seatClass, "economy") == 0 && (row >= 11 && row <= 20))
     {
         printf("You are in Group C!");
     }
@@ -27,5 +32,6 @@ int main()
     {
         printf("You are in Group D!");
     }
+
     return 0;
 }
