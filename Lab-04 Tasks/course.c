0965c1b9a6405362a725c3fe96c32b4d656e88fe
Muhammed Ahmed Ashfaq <114571771+MDAhmed115@users.noreply.github.com>
2025-09-26 15:17:09 +0500

    Update course.c

--- Lab-04 Tasks/course.c
+++ Lab-04 Tasks/course.c
@@ -1,52 +1,72 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
     int coursecount = 0;
-    char course[100000];
-    char pass[100000];
-    char overlap[10000];
-    char timecheck[10000];
-    char newcourse[10000];
+    int check = 0;
+    char course[1000];
+    char pass[100];
+    char overlap[100];
+    char timecheck[100];
+    char newcourse[100];
+
     printf("How many courses you already have? ");
     scanf("%d", &coursecount);
+
     printf("\nWould you like to take a new course (yes/no)? ");
     scanf("%s", newcourse);
-    while (coursecount < 5 && strcmp(newcourse, "yes") == 0 && coursecount > 0)
+
+    while (coursecount < 5 && strcmp(newcourse, "yes") == 0 && check == 0)
     {
         printf("\nWhich course you would like to take? ");
-        scanf("%s", &course);
-        printf("\nHave you passed all the subjects (yes/or)? ");
-        scanf("%s", &pass);
-        printf("\nIs there any other course on same time (yes/no)? If yes which one? \n");
-        scanf("%s", &timecheck);
+        scanf("%s", course);
+
+        printf("\nHave you passed all the subjects (yes/no)? ");
+        scanf("%s", pass);
+
+        printf("\nIs there any other course at the same time (yes/no)? ");
+        scanf("%s", timecheck);
+
         if (strcmp(timecheck, "yes") == 0)
         {
-            scanf("%s", &overlap);
+            printf("Which course does it overlap with? ");
+            scanf("%s", overlap);
         }
+
         if (strcmp(pass, "yes") == 0)
         {
             if (strcmp(timecheck, "no") == 0)
             {
                 coursecount++;
                 printf("\nYou have selected %s course", course);
-                printf("\nWould you like to take another course (yes/or)? ");
-                scanf("%s", &newcourse);
+
+                if (coursecount < 5) // ask again only if not full
+                {
+                    printf("\nWould you like to take another course (yes/no)? ");
+                    scanf("%s", newcourse);
+                }
             }
             else if (strcmp(timecheck, "yes") == 0)
             {
-                printf("\n%s course overlap with %s course", course, overlap);
+                printf("\n%s course overlaps with %s course", course, overlap);
+                check = 1; // stop further selection
+                break;
             }
         }
         else
         {
             printf("\nYou have not passed all the subjects for this course");
+            check = 1; // stop further selection
+            break;
         }
     }
+
     if (coursecount == 5)
     {
         printf("\nMaximum course limit reached!");
     }
+
     printf("\nYou have selected %d courses", coursecount);
     return 0;
 }
